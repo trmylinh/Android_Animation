633cdb716c95cc51c1b7419ede0585f09c95708f
trmylinh <linhninh2@gmail.com>
2024-11-18 11:58:07 +0700

    add lottie animation (splash)

---
+++ app/src/main/java/com/example/android_animation/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.android_animation
+
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_animation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var isAnimation: AnimationDrawable
+    var isStart = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.apply {
+            img.setImageResource(R.drawable.animation_item)
+            isAnimation = img.drawable as AnimationDrawable
+
+            btn.setOnClickListener {
+                if(!isStart){
+                    isAnimation.start()
+                    btn.setBackgroundColor(Color.RED)
+                    isStart = true
+                    btn.text = "Stop"
+                }else{
+                    isAnimation.stop()
+                    btn.setBackgroundColor(resources.getColor(R.color.green, null))
+                    btn.text = "Start"
+                    isStart = false
+                }
+            }
+
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android_animation/splashscreen/SplashActivity.kt
@@ -0,0 +1,46 @@
+package com.example.android_animation.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_animation.MainActivity
+import com.example.android_animation.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
